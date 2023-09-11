3960a986396148ae7dbc9bea083d577572e06c8b
Ceder <ceder.dens@uantwerpen.be>
2023-09-11 18:18:43 +0200

    Removing duplicates with many options to merge other columns. Filtering rows. Split data on modification, mod/unmod, and column value

--- src/data.py
+++ src/data.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import pyteomics.proforma
 
+
 # TODO: support more filetypes
-from src.util import add_length
-
-
 def read_file(filepath, filetype='csv', sep=',', index_col=0):
     if filetype == 'csv':
-        df = pd.read_csv(open(filepath, 'r'), sep=sep, index_col=index_col)
+        df = pd.read_csv(filepath, sep=sep, index_col=index_col)
     else:
         raise NotImplementedError("Only csv files are supported right now")
     df = df.reset_index(drop=True)
@@ -27,7 +25,73 @@ def parse_modifications(df, mod_seq_col, proforma_col='proforma', unmod_seq_col=
     return df
 
 
-if __name__ == "__main__":
-    df = read_file('data/iRT_unimod_unfiltered.csv')
-    df = parse_modifications(df, 'modified_sequence')
-    df = add_length(df)
+def merge_duplicates(df, how, col='proforma', to_merge=None, merge_val_col=None):
+    """
+    Merge duplicate records based on `col`.
+    :param df: Dataframe to merge the records from
+    :param how: {'mean', 'median', 'col_min', 'col_max'}, how to handle duplicate values in `col`. All column values in
+    `to_merge` can be merged by taking the 'mean' or 'median'. Alternatively 'col_min' and 'col_max' merge by taking the
+     record with the min or max value of an additional column `merge_val_col`
+    :param col: column for which to merge duplicates
+    :param to_merge: single column name or list of column names to merge with the `how` practice
+    :param merge_val_col: Only used when how is 'col_min' or 'col_max'. Additional column from which the min or max
+    value will be computed and the corresponding record will be chosen when merging duplicates.
+    :return: merged Dataframe
+    """
+    if to_merge is None:
+        to_merge = []
+    if isinstance(to_merge, str):
+        to_merge = [to_merge]
+
+    other_cols = [c for c in df.columns if c not in to_merge]
+
+    if how == 'mean':
+        merged_vals = df.groupby(by=col)[to_merge].mean()
+    elif how == 'median':
+        merged_vals = df.groupby(by=col)[to_merge].median()
+
+    elif how == 'col_min':
+        if merge_val_col is None:
+            raise ValueError("Argument 'merge_val_col' must be given when how is col_min or col_max")
+        merged_vals = df.groupby(by=col).min(merge_val_col)[to_merge]
+    elif how == 'col_max':
+        if merge_val_col is None:
+            raise ValueError("Argument 'merge_val_col' must be given when how is col_min or col_max")
+        merged_vals = df.groupby(by=col).max(merge_val_col)[to_merge]
+    else:
+        raise ValueError("Possible values for argument 'how' of remove_duplicates are 'mean', 'median', or 'col'")
+
+    df = df[other_cols].drop_duplicates(col)
+    df = pd.merge(df[other_cols], merged_vals, how='outer', left_on=col, right_index=True,
+                  validate='1:1').reset_index(drop=True)
+    return df
+
+
+def filter_rows(df, how, col, val):
+    if how == 'lt':
+        return df[df[col] < val]
+
+    elif how == 'lte':
+        return df[df[col] <= val]
+
+    elif how == 'gte':
+        return df[df[col] >= val]
+
+    elif how == 'gt':
+        return df[df[col] > val]
+
+    elif how == 'eq':
+        return df[df[col] == val]
+
+    elif how == 'neq':
+        return df[df[col] != val]
+
+
+def export(df, filepath, as_type='csv', cols=None):
+    if cols is not None:
+        df = df[cols]
+
+    if as_type == 'csv':
+        if not filepath.endswith('.csv'):
+            filepath += '.csv'
+        df.to_csv(filepath)
--- src/util.py
+++ src/util.py
@@ -1,10 +1,119 @@
+import pandas as pd
+
+
 def add_length(df, len_col='length', unmod_seq_col='unmodified_sequence'):
     df[len_col] = df[unmod_seq_col].str.len()
     return df
 
 
-def split(df, split_type):
+def num_modifications(proforma_tuple):
+    mod_count = sum(1 for s in proforma_tuple[0] if s[1] is not None)
+    n_term = proforma_tuple[1]['n_term']
+    c_term = proforma_tuple[1]['c_term']
+
+    if n_term is not None:
+        mod_count += len(n_term)
+
+    if c_term is not None:
+        mod_count += len(c_term)
+
+    return mod_count
+
+
+def filter_num_mods(df, num=None, proforma_col='proforma', add_to_df=False):
+    if num is None:
+        raise ValueError('num must be an int or a list of ints')
+
+    if isinstance(num, int):
+        num = [num]
+
+    num_mods = df[proforma_col].apply(num_modifications)
+
+    if add_to_df:
+        df['num_mods'] = num_mods
+
+    return pd.concat([df[num_mods == n] for n in num])
+
+
+def split_mod_unmod(df, proforma_col='proforma'):
+    if 'num_mods' not in df.columns:
+        num_mods = df[proforma_col].apply(num_modifications)
+    else:
+        num_mods = df['num_mods']
+
+    return df[num_mods > 0], df[num_mods == 0]
+
+
+def split_col(df, col_name):
+    return [d for _, d in df.groupby(col_name)]
+
+
+def has_mod(proforma_tuple, mod):
+    for p in proforma_tuple[0]:
+        if p[1] is not None:
+            for m in p[1]:
+                if m.value == mod:
+                    return True
+    n_term = proforma_tuple[1]['n_term']
+    c_term = proforma_tuple[1]['c_term']
+    if n_term is not None:
+        for n in n_term:
+            if n.value == mod:
+                return True
+    if c_term is not None:
+        for c in c_term:
+            if c.value == mod:
+                return True
+    return False
+
+
+def get_all_mods(proforma_col):
+    all_mods = set()
+    for p in proforma_col:
+        for aa, m in p[0]:
+            if m is not None:
+                all_mods.update(n.value for n in m)
+
+        n_term = p[1]['n_term']
+        c_term = p[1]['c_term']
+        if n_term is not None:
+            all_mods.update(n.value for n in n_term)
+        if c_term is not None:
+            all_mods.update(c.value for c in c_term)
+
+    return all_mods
+
+
+def split_mods(df, proforma_col='proforma', single_mod_only=True, keep_same_unmodified_sequence=True,
+               unmod_seq_col='unmodified_sequence'):
+    if single_mod_only:
+        df = filter_num_mods(df, [0, 1], proforma_col, add_to_df=True)
+        unmod_records = df[df['num_mods'] == 0]
+    else:
+        unmod_records = filter_num_mods(df, 0, proforma_col)
+
+    all_mods = get_all_mods(df[proforma_col])
+    mod_dfs = {}
+    for m in all_mods:
+        mod_df = df[df[proforma_col].apply(has_mod, args=(m,))]
+        if keep_same_unmodified_sequence:
+            unmod_seqs = unmod_records[unmod_records[unmod_seq_col].isin(mod_df[unmod_seq_col])]
+            mod_df = pd.concat([mod_df, unmod_seqs])
+        mod_dfs[m] = mod_df
+
+    return mod_dfs
+
+
+def split(df, split_type, proforma_col='proforma', single_mod_only=False, keep_same_unmodified_sequence=False,
+          unmod_seq_col='unmodified_sequence', col_name=None):
     if split_type == 'mod_unmod':
-        pass
+        ret = split_mod_unmod(df, proforma_col=proforma_col)
+    elif split_type == 'mods':
+        ret = split_mods(df, proforma_col=proforma_col, single_mod_only=single_mod_only,
+                         keep_same_unmodified_sequence=keep_same_unmodified_sequence, unmod_seq_col=unmod_seq_col)
     elif split_type == 'col':
-        raise NotImplementedError('Splitting on column value is not yet implemented')
+        ret = split_col(df, col_name)
+    else:
+        raise NotImplementedError(f'Unexpected value for split_type: {split_type}')
+
+    return ret
