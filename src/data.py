98f990b8f0bbcd61aae96c5b5aa764aeda5d9157
Ceder <ceder.dens@uantwerpen.be>
2023-09-11 12:05:13 +0200

    todo added

--- src/data.py
+++ src/data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 
+# TODO: support more filetypes
 def read_file(filepath, filetype='csv', sep=',', index_col=0):
     if filetype == 'csv':
         df = pd.read_csv(open(filepath, 'r'), sep=sep, index_col=index_col)
